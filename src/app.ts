d6b9ce92515d7e7a93e8f93ed6ae833ade298d11
Aleksandr Dzyubinskiy <timik_333@mail.ru>
2023-05-29 18:27:22 +0500

    feat: add limit the complexity of the graphql queries.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import AutoLoad from '@fastify/autoload';
-import { FastifyPluginAsync } from 'fastify';
+import {FastifyPluginAsync} from 'fastify';
+import * as depthLimit from "graphql-depth-limit";
 
 const app: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.register(AutoLoad, {
@@ -11,6 +12,20 @@ const app: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
     options: {},
+  });
+
+  fastify.addHook('preValidation', async (request: any, reply: any) => {
+    if (request.body?.operationName === 'graphql') {
+      const maxDepth = 5;
+      try {
+        depthLimit(
+          maxDepth,
+          { ignore: [ 'introspectionQuery' ] },
+        )(request, reply);
+      } catch (error: any) {
+        reply.send({ errors: [{ message: error.message }] });
+      }
+    }
   });
 };
 
